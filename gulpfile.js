919ec19ce04b38528836790973fe9ead224cbf5e
Martin Weingart <martinweingart@gmail.com>
2017-07-08 10:53:02 -0300

    Ruta / para servidor node

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,19 @@ const gulp = require('gulp');
 const path = require('path');
 const scp = require('gulp-scp2');
 const replace = require('gulp-replace-task');
+const argv = require('minimist')(process.argv.slice(2));
 
-gulp.task('replace', function() {
-  gulp.src(['dist/**/*'])
+gulp.task('sethost', function() {
+  gulp.src(['node-server/index.js', 'dist/**/*'])
     .pipe(replace({
             patterns: [
               {
                 match: new RegExp('http://localhost:3000', "g"),
                 replacement: function() {
-                  return `http://${process.argv[2]}:3000`;
+                  return `http://${argv.h}`;
                 }
               },
-              {
-                match: new RegExp('/static/', "g"),
-                replacement: function() {
-                  return '/libreplay/static/'
-                }
-              }
             ]
           }))
     .pipe(gulp.dest('client/dist/'));
-});
-
-gulp.task('copy', function() {
-  return gulp.src(['client/dist/**/*.*'])
-  .pipe(scp({
-    host: process.argv[2],
-    username: process.argv[3],
-    password: process.argv[4],
-    dest: process.argv[5]
-  }))
-  .on('error', function(err) {
-    console.log(err);
-  });
 });
--- node-server/index.js
+++ node-server/index.js
@@ -1,21 +1,23 @@
 const path = require('path');
-const app = require('express')();
-const server = require('http').Server(app);
+const express = require('express');
 const mpd = require('mpd');
 const db = require('./db');
 const utils = require('./utils');
 const rtypes = require('./rtypes');
 const body_parser = require('body-parser');
 
+const app = express();
+const server = require('http').Server(app);
 server.listen(3000);
-
 let io = require('socket.io')(server);
-let client = mpd.connect({
+
+let mpd_client = mpd.connect({
   port: 6600,
   host: '10.1.38.88',
 });
 
 app.use(body_parser.json());
+app.use('/', express.static(path.join(__dirname, '..', 'dist')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -25,26 +27,26 @@ app.use(function(req, res, next) {
 });
 
 io.on('connection', function (socket) {
-  client.on('ready', function() {
+  mpd_client.on('ready', function() {
     socket.emit('ready');
   });
 
-  client.on('system', function(name) {
+  mpd_client.on('system', function(name) {
     // console.log("update", name);
   });
 
-  client.on('system-player', function() {
+  mpd_client.on('system-player', function() {
     socket.emit('update-player');
   });
 
-  client.on('system-playlist', function() {
+  mpd_client.on('system-playlist', function() {
     socket.emit('update-playlist');
   });
 });
 
 app.post('/command', function(req, res) {
   let params = req.body.params ? req.body.params : [];
-  client.sendCommand(mpd.cmd(req.body.command, params), function(err, msg) {
+  mpd_client.sendCommand(mpd.cmd(req.body.command, params), function(err, msg) {
     if (err) {
       console.log(err);
       rtypes.internal_error(res);
@@ -54,14 +56,14 @@ app.post('/command', function(req, res) {
 });
 
 app.get('/status', function(req, res) {
-  client.sendCommand(mpd.cmd('status', []), function(err, msg) {
+  mpd_client.sendCommand(mpd.cmd('status', []), function(err, msg) {
     if (err) console.log('status error:', err);
     rtypes.ok(res, utils.formatListMulti(msg, 'volume')[0]);
   });
 });
 
 app.get('/playlist', function(req, res) {
-  client.sendCommand(mpd.cmd('playlistinfo', []), function(err, msg) {
+  mpd_client.sendCommand(mpd.cmd('playlistinfo', []), function(err, msg) {
     if (err) console.log('playlist error:', err);
     let playlist = utils.formatListMulti(msg, 'file');
     if (playlist.length == 0 || !playlist[0]) playlist = [];
@@ -70,7 +72,7 @@ app.get('/playlist', function(req, res) {
 });
 
 app.post('/playlist/add', function(req, res) {
-  client.sendCommand(mpd.cmd('addid', [req.body.uri]), function(err, msg) {
+  mpd_client.sendCommand(mpd.cmd('addid', [req.body.uri]), function(err, msg) {
     if (err) console.log(err);
     let result = utils.formatList(msg)[0];
     rtypes.ok(res, result);
@@ -78,7 +80,7 @@ app.post('/playlist/add', function(req, res) {
 });
 
 app.get('/database/artists', function(req, res) {
-  db.getArtists(client)
+  db.getArtists(mpd_client)
     .then(r => rtypes.ok(res, r))
     .catch(e => {
       console.log(e);
@@ -87,7 +89,7 @@ app.get('/database/artists', function(req, res) {
 });
 
 app.get('/database/songs', function(req, res) {
-  db.getSongs(client, req.query)
+  db.getSongs(mpd_client, req.query)
     .then(r => rtypes.ok(res, r))
     .catch(e => {
       console.log(e);
